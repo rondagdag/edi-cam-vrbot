222c2ae51fdb21807cf4915ad253b7f95b779935
Ron Dagdag <rlyle78@gmail.com>
2016-08-10 21:13:19 +0000

    move aframe-html-shader to browserify

--- web/server/browser.js
+++ web/server/browser.js
@@ -1,6 +1,7 @@
 window.Leap = require('leapjs');
 window.AFRAME = require('aframe');
 window.AFRAME.extras = require('aframe-extras');
+require('aframe-html-shader');
 
 window.AFRAME.extras.registerAll();
 require('aframe-leap-hands').registerAll();
